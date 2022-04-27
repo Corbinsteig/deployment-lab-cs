57241c6d086a21ee35a1cd81ce9d8687482958ff
Corbin Steig <corsteig1@gmail.com>
2022-04-27 13:28:23 -0700

    commit three

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-const port = process.env.PORT || 4005
+const port = process.env.PORT || 5501
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
