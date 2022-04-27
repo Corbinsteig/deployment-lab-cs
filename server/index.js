31a8ebcfb3cc6f002210e6331d4e020143034a0d
Corbin Steig <corsteig1@gmail.com>
2022-04-27 13:45:27 -0700

    fourth real commit

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../landingpage.html'))
 })
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../landingpage.css'))
+})
+
 const port = process.env.PORT || 5501
 
 app.listen(port, () => {
